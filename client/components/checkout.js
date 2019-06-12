8d6b5daded44684423d4c6b77523f2da9c90b488
EricBot89 <eloucks@gmail.com>
2019-06-11 20:49:29 -0400

    checkout works as a stateless comp fine, breaks as a class comp

--- client/components/checkout.js
+++ client/components/checkout.js
@@ -37,19 +37,21 @@ const invoiceTotal = invoiceTaxes + invoiceSubtotal
 
 const TAX_RATE = 0.07
 
-class checkoutMenu extends React.Component {
-  constructor(props) {
+const checkoutMenu = props => {
+  /* 
+    constructor(props) {
     super(props)
     this.state = {
-      reciepientName,
-      confirmationEmail,
-      price,
-      address,
-      city,
-      state,
-      zipcode,
-      userId
+      reciepientName: "",
+      confirmationEmail: "",
+      price: 0,
+      address: "",
+      city: "",
+      state: "",
+      zipcode: "",
+      userId: 0
     }
+    this.changeHandler = this.changeHandler.bind(this)
   }
 
   changeHandler(e) {
@@ -59,145 +61,76 @@ class checkoutMenu extends React.Component {
     message[feildName] = newVal
     this.setState(message)
   }
+*/
+  // render() {
+  const classes = useStyles()
+  //   if (this.props.isLoggedIn) {
+  //      const recipientName = this.props.user.firstName
+  //      const confirmationEmail = this.props.user.email
+  //     const userId = this.props.user.id
+  const beforeTax = invoiceSubtotal
+  const tax = invoiceTaxes
+  const price = invoiceTotal
+  //    this.setState({price})
+  return (
+    <Container maxWidth="md">
+      <Paper>
+        <Grid container alignItems="flex-end" justify="flex-end">
+          <form className={classes.container}>
+            <TextFeild
+              id="address"
+              label="Address"
+              //                 className={classes.textFeild}
+              //                  onChange={(e) => this.changeHandler(e)}
+              //                 value={this.state.address}
+              margin="normal"
+              variant="outlined"
+            />
+            <TextFeild
+              id="city"
+              label="City"
+              //                 className={classes.textFeild}
+              //                onChange={(e) => this.changeHandler(e)}
 
-  render() {
-    const classes = useStyles()
-    if (props.isLoggedIn) {
-      this.setState({recipientName: props.user.firstName})
-      this.setState({confirmationEmail: props.user.email})
-      this.setState({userId: props.user.id})
-      const beforeTax = invoiceSubtotal
-      const tax = invoiceTaxes
-      const price = invoiceTotal
-      this.setState({price})
-      return (
-        <Container maxWidth="md">
-          <Paper>
-            <Grid container alignItems="flex-end" justify="flex-end">
-              <form className={classes.container} autoComplete="on">
-                <TextFeild
-                  id="address"
-                  label="Address"
-                  className={classes.textFeild}
-                  value={this.state.address}
-                  margin="normal"
-                  variant="outlined"
-                />
-                <TextFeild
-                  id="city"
-                  label="City"
-                  className={classes.textFeild}
-                  value={this.state.city}
-                  margin="normal"
-                  variant="outlined"
-                />
-                <TextFeild
-                  id="state"
-                  label="State"
-                  className={classes.textFeild}
-                  value={this.state.state}
-                  margin="normal"
-                  variant="outlined"
-                />
-                <TextFeild
-                  id="zipcode"
-                  label="Zipcode"
-                  className={classes.textFeild}
-                  value={this.state.zipcode}
-                  margin="normal"
-                  variant="outlined"
-                />
+              //                 value={this.state.city}
+              margin="normal"
+              variant="outlined"
+            />
+            <TextFeild
+              id="state"
+              label="State"
+              //                className={classes.textFeild}
+              //                onChange={(e) => this.changeHandler(e)}
 
-                <Button
-                  variant="contained"
-                  className={classes.button}
-                  color="primary"
-                  type="submit"
-                >
-                  Submit Order
-                </Button>
-              </form>
-            </Grid>
-          </Paper>
-        </Container>
-      )
-    } else {
-      const beforeTax = invoiceSubtotal
-      const tax = invoiceTaxes
-      const price = invoiceTotal
-      this.setState({price})
-      return (
-        <Container maxWidth="md">
-          <Paper>
-            <Grid container alignItems="flex-end" justify="flex-end">
-              <form className={classes.container} autoComplete="on">
-                <Grid item />
-                <TextField
-                  id="outlined-email-input"
-                  label="Email"
-                  className={classes.textField}
-                  value={this.state.confirmationEmail}
-                  type="email"
-                  name="email"
-                  autoComplete="email"
-                  margin="normal"
-                  variant="outlined"
-                />
-                <TextFeild
-                  id="reciepientName"
-                  label="Name"
-                  className={classes.textFeild}
-                  value={this.state.reciepientName}
-                  margin="normal"
-                  variant="outlined"
-                />
-                <TextFeild
-                  id="address"
-                  label="Address"
-                  className={classes.textFeild}
-                  value={this.state.address}
-                  margin="normal"
-                  variant="outlined"
-                />
-                <TextFeild
-                  id="city"
-                  label="City"
-                  className={classes.textFeild}
-                  value={this.state.city}
-                  margin="normal"
-                  variant="outlined"
-                />
-                <TextFeild
-                  id="state"
-                  label="State"
-                  className={classes.textFeild}
-                  value={this.state.state}
-                  margin="normal"
-                  variant="outlined"
-                />
-                <TextFeild
-                  id="zipcode"
-                  label="Zipcode"
-                  className={classes.textFeild}
-                  value={this.state.zipcode}
-                  margin="normal"
-                  variant="outlined"
-                />
-                <Button
-                  variant="contained"
-                  className={classes.button}
-                  color="primary"
-                  type="submit"
-                >
-                  Submit Order
-                </Button>
-              </form>
-            </Grid>
-          </Paper>
-        </Container>
-      )
-    }
-  }
+              //                value={this.state.state}
+              margin="normal"
+              variant="outlined"
+            />
+            <TextFeild
+              id="zipcode"
+              label="Zipcode"
+              className={classes.textFeild}
+              //              onChange={(e) => this.changeHandler(e)}
+              //              value={this.state.zipcode}
+              margin="normal"
+              variant="outlined"
+            />
+
+            <Button
+              variant="contained"
+              className={classes.button}
+              color="primary"
+              type="submit"
+            >
+              Submit Order
+            </Button>
+          </form>
+        </Grid>
+      </Paper>
+    </Container>
+  )
+  //   }
+  // }
 }
 
 const mapState = state => {
