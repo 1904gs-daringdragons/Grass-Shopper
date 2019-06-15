b3b147fe3f6e9d08cca504aa495a6736875020d7
Sunny Chang Mei <mkaleader@gmail.com>
2019-06-15 16:11:37 -0400

    add button for history, useredit, reset password

--- client/components/account-details.js
+++ client/components/account-details.js
@@ -1,16 +1,36 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
-import {me} from '../store/user'
+import {me, displayUserInfoName} from '../store/user'
+import PropTypes from 'prop-types'
+import {Button, Grid} from '@material-ui/core'
+import {Link} from 'react-router-dom'
 
 class AccountDetails extends Component {
   async componentDidMount() {
     await this.props.getUser()
   }
-
   render() {
-    const {user} = this.props
+    const {user, displayUserName} = this.props
     return user.id ? (
       <div className="center">
+        <h2>Welcome, {displayUserName}</h2>
+        <Grid container spacing={2}>
+          <Link to="/profile/orders">
+            <Button variant="contained" color="secondary">
+              View Your Orders
+            </Button>
+          </Link>
+          <Link to="/profile/update">
+            <Button variant="contained" color="secondary">
+              Update Your Profile
+            </Button>
+          </Link>
+          <Link to="profile/resetPassword">
+            <Button variant="contained" color="secondary">
+              Reset Your Password
+            </Button>
+          </Link>
+        </Grid>
         <br />
         <h1>Account Details</h1>
         <br />
@@ -23,12 +43,15 @@ class AccountDetails extends Component {
         <h3>Zipcode:</h3> <p> {user.zipcode}</p>
       </div>
     ) : (
-      <div>Loading user details...</div>
+      <div>
+        <h3> Loading user details...</h3>
+      </div>
     )
   }
 }
 
 const mapState = state => ({
+  displayUserName: displayUserInfoName(state.user.firstName, state.user.email),
   user: state.user
 })
 
@@ -37,3 +60,68 @@ const mapDispatch = () => ({
 })
 
 export default connect(mapState, mapDispatch)(AccountDetails)
+
+AccountDetails.propTypes = {
+  displayUserName: PropTypes.string
+}
+
+// import React from 'react'
+// import PropTypes from 'prop-types'
+// import {connect} from 'react-redux'
+// import {displayUserInfoName} from '../store/user'
+// // import AdminHome from './AdminHome'
+// import {Link} from 'react-router-dom'
+// import {Button, ButtonToolbar} from '@material-ui/core'
+// // import DisplayPastOrders from './DisplayPastOrders'
+
+// /**
+//  * COMPONENT
+//  */
+// export const AccountDetails = props => {
+//   const {displayName} = props
+
+//   return (
+//     <div>
+//       <br />
+//       <h4>Welcome, {displayName}</h4>
+//       {/* <ButtonToolbar> */}
+//         <Link to="/profile/orders">
+//           <Button variant="secondary" className="home-page-btn">
+//             View Your Orders
+//           </Button>
+//         </Link>
+
+//         <Link to="profile/update">
+//           <Button variant="secondary" className="home-page-btn">
+//             Update Your Profile Information
+//           </Button>
+//         </Link>
+
+//         <Link to="profile/resetPassword">
+//           <Button variant="secondary" className="home-page-btn">
+//             Reset Your Password
+//           </Button>
+//         </Link>
+//       {/* </ButtonToolbar> */}
+//     </div>
+//   )
+// }
+
+// /**
+//  * CONTAINER
+//  */
+// const mapState = state => {
+//   return {
+//     displayName: displayUserInfoName(state.user.firstName, state.user.email),
+//     user: state.user
+//   }
+// }
+
+// export default connect(mapState)(AccountDetails)
+
+// /**
+//  * PROP TYPES
+//  */
+// AccountDetails.propTypes = {
+//   displayName: PropTypes.string
+// }
