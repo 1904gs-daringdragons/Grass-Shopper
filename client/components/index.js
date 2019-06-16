7e4695cd2626b8451afc7ee6a0b02fe623d0b6f6
EricBot89 <eloucks@gmail.com>
2019-06-16 13:06:27 -0400

    updates order page to allow editing by admin and cancelation of orders in both user and admin view. also adds orders to seed file for testing

--- client/components/index.js
+++ client/components/index.js
@@ -4,7 +4,7 @@
  * having to figure out which file they belong to!
  */
 export {default as Navbar} from './navbar'
-export {default as UserHome} from './user-home'
+export {default as UserHome} from './UserHome'
 export {default as Products} from './product-list'
 export {Login, Signup} from './auth-form'
 export {default as Cart} from './cart'
