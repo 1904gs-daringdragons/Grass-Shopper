db0758312f5cf528cd60025e203acafeb10579ce
EricBot89 <eloucks@gmail.com>
2019-06-16 15:45:00 -0400

    adds typography and papaer to userdetails and links up user order view with the button there

--- client/components/account-details.js
+++ client/components/account-details.js
@@ -2,7 +2,14 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {me, displayUserInfoName} from '../store/user'
 import PropTypes from 'prop-types'
-import {Button, Grid, Paper, Card} from '@material-ui/core'
+import {
+  Button,
+  Grid,
+  Paper,
+  Card,
+  CardContent,
+  Typography
+} from '@material-ui/core'
 import {Link} from 'react-router-dom'
 import {makeStyles} from '@material-ui/core/styles'
 
@@ -15,7 +22,9 @@ class AccountDetails extends Component {
     return user.id ? (
       <Paper>
         <div className="center">
-          <h2>Welcome, {displayUserName}</h2>
+          <Typography>
+            <h2>Welcome, {displayUserName}</h2>
+          </Typography>
           <Grid container spacing={2}>
             <Link to="/orders">
               <Button variant="contained" color="secondary">
@@ -34,15 +43,20 @@ class AccountDetails extends Component {
             </Link>
           </Grid>
           <br />
-          <h1>Account Details</h1>
-          <br />
-          <h3>First Name:</h3> <p> {user.firstName}</p>
-          <h3>Last Name:</h3> <p> {user.lastName}</p>
-          <h3>Email Address:</h3> <p> {user.email}</p>
-          <h3>Billing Address:</h3> <p> {user.billingAddress}</p>
-          <h3>City:</h3> <p> {user.billingCity}</p>
-          <h3>State:</h3> <p> {user.billingState}</p>
-          <h3>Zipcode:</h3> <p> {user.billingZipcode}</p>
+          <Card>
+            <CardContent>
+              <Typography>
+                <h2>Account Details</h2>
+                <h3>First Name:</h3> <p> {user.firstName}</p>
+                <h3>Last Name:</h3> <p> {user.lastName}</p>
+                <h3>Email Address:</h3> <p> {user.email}</p>
+                <h3>Billing Address:</h3> <p> {user.billingAddress}</p>
+                <h3>City:</h3> <p> {user.billingCity}</p>
+                <h3>State:</h3> <p> {user.billingState}</p>
+                <h3>Zipcode:</h3> <p> {user.billingZipcode}</p>
+              </Typography>
+            </CardContent>
+          </Card>
         </div>
       </Paper>
     ) : (
