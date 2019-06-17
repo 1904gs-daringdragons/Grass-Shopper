762368e4a743fed1dc8bf9d60bd7263b7814821c
msicil <mikejoesis@gmail.com>
2019-06-17 17:08:56 -0400

    added some style changes and all of erics changes

--- client/components/ProductManager.js
+++ client/components/ProductManager.js
@@ -1,13 +1,60 @@
 import React from 'react'
 import {connect} from 'react-redux'
-import {
-  Container,
-  Paper,
-  Table,
-  TableBody,
-  TableHead,
-  TableCell,
-  TableRow
-} from '@material-ui/core'
+import MaterialTable from 'material-table'
+import {addProductThunk, updateProductThunk, deleteProductThunk} from '../store'
+import axios from 'axios'
 
-export default connect(null, null)(ProductManager)
+class ProductManager extends React.Component {
+  render() {
+    return (
+      <MaterialTable
+        title="Product Manager"
+        columns={[
+          {title: 'Name', field: 'name'},
+          {title: 'Price', field: 'price', type: 'numeric'},
+          {title: 'Description', field: 'description'}
+        ]}
+        data={async query => {
+          const res = await axios.get('/api/products')
+          const tableData = res.data
+          const tableRowCt = tableData.length
+          const {pageSize, page} = query
+          const renderData = tableData.slice(
+            page * pageSize,
+            page * pageSize + pageSize
+          )
+          return {
+            data: renderData,
+            page: page,
+            totalCount: tableRowCt
+          }
+        }}
+        editable={{
+          onRowAdd: async newData => {
+            this.props.addProduct(newData)
+          },
+          onRowUpdate: async (newData, oldData) => {
+            const {id} = oldData
+            this.props.updateProduct(id, newData)
+          },
+          onRowDelete: async oldData => {
+            const {id} = oldData
+            this.props.deleteProduct(id)
+          }
+        }}
+      />
+    )
+  }
+}
+
+const mapState = state => ({
+  productList: state.products.allProducts
+})
+
+const mapDispatch = dispatch => ({
+  addProduct: newData => dispatch(addProductThunk(newData)),
+  updateProduct: (pId, newData) => dispatch(updateProductThunk(pId, newData)),
+  deleteProduct: pId => dispatch(deleteProductThunk(pId))
+})
+
+export default connect(mapState, mapDispatch)(ProductManager)
