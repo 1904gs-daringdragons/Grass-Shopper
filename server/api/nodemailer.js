b78f7f9f52b7a7aecef99377cb5d7b79a9ecfefc
Nick B <nicholas.balestrieri@gmail.com>
2019-06-18 13:45:42 -0400

    missed one change

--- server/api/nodemailer.js
+++ server/api/nodemailer.js
@@ -3,10 +3,6 @@ const nodemailer = require('nodemailer')
 
 // async..await is not allowed in global scope, must use a wrapper
 async function main() {
-  // Generate test SMTP service account from ethereal.email
-  // Only needed if you don't have a real mail account for testing
-  let testAccount = await nodemailer.createTestAccount()
-
   // create reusable transporter object using the default SMTP transport
   let transporter = nodemailer.createTransport({
     host: 'email',
